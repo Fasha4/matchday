ca54943dc245e686c3c891fb1da4ac737927dad5
Fabian Eschik <dafbian4@gmail.com>
2024-05-08 15:37:14 +0200

    added time synchro with flashscore

--- Viaplay/viaplay_scraper.py
+++ Viaplay/viaplay_scraper.py
@@ -53,10 +53,13 @@ def getMatches(custom_date):
 		if league not in leagues:
 			leagues.append(league)
 
-		ActionChains(driver).move_to_element_with_offset(match, 145, -70).perform()
-		wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".MetaWithIcon-module-text-GhqPn")))
-		comment = driver.find_element(By.CSS_SELECTOR, ".MetaWithIcon-module-text-GhqPn").text
-		comment = ", ".join(comment.split(' & '))
+		ActionChains(driver).move_to_element_with_offset(match, 140, -70).perform()
+		wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".Tooltip-module-title-St0-w")))
+		try:
+			comment = driver.find_element(By.CSS_SELECTOR, ".MetaWithIcon-module-text-GhqPn").text
+			comment = ", ".join(comment.split(' & '))
+		except:
+			comment = ""
 		ActionChains(driver).move_by_offset(-50, 0).perform()
 
 		matches.append({
@@ -89,6 +92,63 @@ def getMatches(custom_date):
 
 	return games
 
+def correct_time(matches, custom_date):
+	options = webdriver.ChromeOptions()
+	options.add_argument('--headless')
+	options.add_argument('--window-size=1920,1080')
+	options.add_experimental_option('excludeSwitches', ['enable-logging'])
+	options.add_argument('log-level=3')
+	driver = webdriver.Chrome(service=ChromeService(), options=options)
+
+	driver.get("https://www.flashscore.pl/")
+
+	wait = WebDriverWait(driver, 10)
+	cookies = wait.until(EC.element_to_be_clickable((By.ID, "onetrust-reject-all-handler")))
+	cookies.click()
+
+	date_diff = datetime.date.fromisoformat(custom_date) - datetime.date.today()
+	for i in range(date_diff.days):
+		driver.find_element(By.CSS_SELECTOR, ".calendar__navigation--tomorrow").click()
+	wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "._simpleText_ghuv4_5._webTypeSimpleText01_ghuv4_11")))
+	wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '._simpleText_ghuv4_5._webTypeSimpleText01_ghuv4_11')))
+
+	expand = driver.find_elements(By.CSS_SELECTOR, "._simpleText_ghuv4_5._webTypeSimpleText01_ghuv4_11")
+	for button in expand:
+		button.click()
+
+	f = open('config.json', 'r', encoding='utf-8')
+	config = json.load(f)
+	f.close()
+
+	matches_fs = driver.find_elements(By.CSS_SELECTOR, '.event__match.event__match--twoLine')
+
+	correct_matches = []
+
+	for match in matches_fs:
+		try:
+			home = match.find_element(By.CSS_SELECTOR, '.event__participant.event__participant--home').text
+			away = match.find_element(By.CSS_SELECTOR, '.event__participant.event__participant--away').text
+			matchtime = match.find_element(By.CSS_SELECTOR, '.event__time').text
+			if home in config["flashscore"]:
+				home = config["flashscore"][home]
+			if away in config["flashscore"]:
+				away = config["flashscore"][away]
+			correct_matches.append({
+				'home': home,
+				'away': away,
+				'time': matchtime})
+		except:
+			pass
+
+	for correct_match in correct_matches:
+		for league in matches:
+			for match in league["matches"]:
+				if match["home"] == correct_match["home"] and match["away"] == correct_match["away"]:
+					match["time"] = correct_match["time"]
+
+	return matches
+
+
 def show(matches):
 
 	output = ''
@@ -97,49 +157,59 @@ def show(matches):
 	f.close()
 
 	sort = [x["name_viaplay"] for x in config["leagues"]]
-	matches = sorted(matches, key=lambda x: sort.index(x["name"]))
 
 	for league in matches:
+		if league['name'] not in sort:
+			print("Dodaj", league['name'], "do configa")
+	matches = list(filter(lambda i: i['name'] in sort, matches))
+
+	matches = sorted(matches, key=lambda x: sort.index(x["name"]))
+	for league in matches:
+		if [x['home'] for x in league['matches']] == ['']:
+			continue
 		new_league = next((sub for sub in config["leagues"] if sub["name_viaplay"] == league["name"]), None)
 
-		try:
-			if new_league["show"]:
-				output += r'<img class="aligncenter wp-image-' + str(new_league["wp_img"]) + r'" src="' + new_league["img"] + r'" alt="" width="' + str(new_league["img_w"]) + r'" height="' + str(new_league["img_h"]) + r'" />' + '\n'
-				output += r'<h2 style="text-align: center;"><span style="font-size: 18pt;"><strong>' + new_league["name_matchday"].upper() + r'</strong></span></h2>' + '\n'
-				for match in league["matches"]:
-					if match["home"]:
-						if match["home"] in config["translate"]:
-							home = config["translate"][match["home"]]
-						else:
-							home = match["home"]
-						if match["away"] in config["translate"]:
-							away = config["translate"][match["away"]]
-						else:
-							away = match["away"]
-						output += match["time"] + r' - <strong>' + home.upper() + r' - ' +  away.upper() + r'</strong>' + '\n'
-					elif match["event"]:
-						output += match["time"] + r' - <strong>' + match["event"].upper() + r'</strong>' + '\n'
+		if new_league["show"]:
+			output += r'<img class="aligncenter wp-image-' + str(new_league["wp_img"]) + r'" src="' + new_league["img"] + r'" alt="" width="' + str(new_league["img_w"]) + r'" height="' + str(new_league["img_h"]) + r'" />' + '\n'
+			output += r'<h2 style="text-align: center;"><span style="font-size: 18pt;"><strong>' + new_league["name_matchday"].upper() + r'</strong></span></h2>' + '\n'
+			for match in league["matches"]:
+				if match["event"] in config["ignore"] or "(" in match["home"] or "(" in match["away"]:
+					continue
+				if match["home"]:
+					if match["home"] in config["translate"]:
+						home = config["translate"][match["home"]]
+					else:
+						home = match["home"]
+					if match["away"] in config["translate"]:
+						away = config["translate"][match["away"]]
+					else:
+						away = match["away"]
+					output += match["time"] + r' - <strong>' + home.upper() + r' - ' +  away.upper() + r'</strong>' + '\n'
+				elif match["event"]:
+					output += match["time"] + r' - <strong>' + match["event"].upper() + r'</strong>' + '\n'
 
-					output += r'<span style="font-size: 10pt;"><img class="emoji" role="img" draggable="false" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f4fa.svg" alt="📺" /> '
-					output += r'Viaplay '
-					if match["comment"] == "Transmisja bez komentarza":
-						match["comment"] = 'brak'
-					if ' ' not in match["comment"]:
-						match["comment"] = match["comment"].lower() #podany tylko język
-					output += r'<img class="emoji" role="img" draggable="false" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f399.svg" alt="🎙" width="16" height="16" /> ' + match["comment"] + r'</span>' + '\n'
-					output += '\n'
-				if new_league["comm"]:
-					output += r'<span style="font-size: 10pt;"><em>' + new_league["comm"] + r'</em></span>' + '\n'
-					output += '\n'
-				output += r'<hr />' + '\n'
-		except:
-			print("Brakuje w config", league["name"])
+				output += r'<span style="font-size: 10pt;"><img class="emoji" role="img" draggable="false" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f4fa.svg" alt="📺" /> '
+				output += r'Viaplay '
+				if match["comment"] == "Transmisja bez komentarza":
+					match["comment"] = 'brak'
+				if ' ' not in match["comment"]:
+					match["comment"] = match["comment"].lower() #podany tylko język
+				output += r'<img class="emoji" role="img" draggable="false" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f399.svg" alt="🎙" width="16" height="16" /> ' + match["comment"] + r'</span>' + '\n'
+				output += '\n'
+			if new_league["comm"]:
+				output += r'<span style="font-size: 10pt;"><em>' + new_league["comm"] + r'</em></span>' + '\n'
+				output += '\n'
+			output += r'<hr />' + '\n'
 	pyperclip.copy(output)
 
 
 if __name__ == '__main__':
 	date = input("Podaj datę (YYYY-MM-DD):")
 	matches = getMatches(date)
+	try:
+		matches = correct_time(matches, date)
+	except:
+		print("Nie udało się zsynchronizować meczów")
 	show(matches)
 	print("Rozpiska została skopiowana do schowka")
 	input()
